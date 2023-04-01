f61cb3f7230707173d5c6393dfbf8e09617193dc
Mateus Bezerra <dev.mateusbezerra@gmail.com>
2023-04-01 11:09:49 -0300

    Initial CRUD

---
+++ nest-api/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { User } from './user/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'nest_api',
+      synchronize: true,
+      entities: [User] // Não é uma boa prática
+    }),
+    GraphQLModule.forRoot(<ApolloDriverConfig>{
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
